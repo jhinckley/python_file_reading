fda2f775feebc54903a06c05560068f6c957e03a
jhinckley <me@johnhinckley.net>
2022-12-08 16:47:51 -0800

    better ways to read a file

---
+++ reading_in_a_list.py
@@ -0,0 +1,12 @@
+# with open('configuration.txt') as file:
+#     my_list = file.read().splitlines()
+#     print(my_list)
+    
+# with open('configuration.txt', 'r') as file:
+#     my_list = file.readlines()
+#     print(my_list)
+    
+with open('configuration.txt', 'r') as file:
+    for line in file:
+        print(line)
+    
